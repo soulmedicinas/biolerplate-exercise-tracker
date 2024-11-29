cb2663435e12554b65f231c4bc47af5f3f198c1c
Fomenko Grigory <G.Fomenko@contractors.crifgroup.com>
2024-11-29 17:48:02 +0400

    init

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
