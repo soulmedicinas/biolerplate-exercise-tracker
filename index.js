271b25d9c097f8932f569fd4df2844a57a47e801
Misaki Michiba <61775829+misakimichiba@users.noreply.github.com>
2025-02-01 12:58:15 +0000

    Connected to MongoDB

--- index.js
+++ index.js
@@ -4,13 +4,56 @@ const cors = require("cors");
 require("dotenv").config();
 require("body-parser");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
 
 app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 //variables
 const userArray = [];
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  _id: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -28,6 +71,8 @@ app.post("/api/users", function (req, res) {
 
   user.username = usrname;
   user._id = idNum;
+  user.log = [];
+  user.count = 0;
 
   userArray.push(user);
 
@@ -37,6 +82,48 @@ app.post("/api/users", function (req, res) {
 // GET request to get list of all users
 app.get("/api/users", function (req, res) {
   res.json(userArray);
+});
+
+// GET request to get exercise logs of a user
+app.get("/api/users/:_id/logs", function (req, res) {
+  let id = "";
+  let user = {};
+
+  for (let i = 0; i < userArray.length; i++) {
+    if (userArray[i]._id == req.params._id) {
+      id = req.params._id;
+
+      //testing
+      //console.log(userArray[i]._id);
+      //console.log("params: " + req.params._id)
+      //console.log(id);
+
+      user = userArray[i];
+
+      break;
+    }
+  }
+
+  if (id == "") {
+    //testing
+    //console.log("error: " + id)
+
+    //res.json({ error: "User ID does not exist" });
+
+    res.json({
+      count: 0,
+      log: [],
+      username: "",
+      _id: "",
+    });
+  } else {
+    res.json({
+      count: 0,
+      log: [],
+      username: "",
+      _id: "",
+    });
+  }
 });
 
 app.post("/api/users/:_id/exercises", function (req, res) {
@@ -67,9 +154,12 @@ app.post("/api/users/:_id/exercises", function (req, res) {
     res.json({ error: "Description is required" });
   } else if (req.body.duration == "" || parseInt(req.body.duration) == NaN) {
     res.json({ error: "Duration is required and needs to be an integer" });
-  } else if (!isNaN(new Date(req.body.date)) == false && req.body.date != "") {
-    res.json({ error: "Date must be a valid date" });
   } else if (req.body.date == "") {
+    /*
+    else if (!isNaN(new Date(req.body.date)) == false && req.body.date != "") {
+    res.json({ error: "Date must be a valid date" });
+  } 
+    */
     let date = new Date();
     let weekday = date.toLocaleString("en-us", { weekday: "short" });
     let year = date.getFullYear();
@@ -84,6 +174,15 @@ app.post("/api/users/:_id/exercises", function (req, res) {
 
     //testing
     //console.log(formattedDate);
+    //console.log(userArray);
+
+    user.log.push({
+      description: user.description,
+      duration: user.duration,
+      date: user.date,
+    });
+
+    user.count += 1;
 
     res.json({
       username: user.username,
@@ -109,6 +208,14 @@ app.post("/api/users/:_id/exercises", function (req, res) {
     user.description = req.body.description;
     user.duration = Number(req.body.duration);
     user.date = formattedDate;
+
+    user.log.push({
+      description: user.description,
+      duration: user.duration,
+      date: user.date,
+    });
+
+    user.count += 1;
 
     res.json({
       username: user.username,
